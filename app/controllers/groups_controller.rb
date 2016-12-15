f0355997866724c2d196d07ac5355aac625bf247
weiyueyan <xiaoguaishou0809@gmail.com>
2016-12-15 20:36:58 +0800

    when created a group, join it automatically

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -20,8 +20,8 @@ def create
   @group = Group.new(group_params)
   @group.user = current_user
   if @group.save
-
-  redirect_to groups_path
+    current_user.join!(@group)
+    redirect_to groups_path
 else
   render :new
 end
