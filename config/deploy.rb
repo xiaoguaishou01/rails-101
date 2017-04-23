128482ddfaeb0767427fe0ec6d4ee87660359ee3
weiyueyan <xiaoguaishou0809@gmail.com>
2017-04-23 16:14:17 +0800

    in deploy add passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,6 @@ set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
+set :passenger_restart_with_touch, true
 # Default value for keep_releases is 5
 # set :keep_releases, 5
